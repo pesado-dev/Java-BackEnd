94f34d22b97a38ff9a0319813526deb42c0c85ca
pesado-dev <123827186+pesado-dev@users.noreply.github.com>
2025-02-13 17:33:39 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+package nivelintermediario;
+public class Main {
+    public static void main(String[] args) {
+        //Criar o ninja Naruto - Naruto é um objeto
+        ninja Naruto = new ninja();
+        Naruto.nome = "Naruto Uzumaki";
+        Naruto.aldeia = "Aldeia da folha";
+        Naruto.idade = 17;
+
+        //Criar o ninja Sasuke - Sasuke é um objeto
+        ninja Sasuke = new ninja();
+        Sasuke.nome = "Sasuke Uchiha";
+        Sasuke.aldeia = "Aldeia da folha";
+        Sasuke.idade = 18;
+
+        //Criar o ninja Sakura - Sakura é um objeto
+        ninja sakura = new ninja();
+        sakura.nome = "Sakura Haruno";
+        sakura.aldeia = "Aldeia da folha";
+        sakura.idade = 18;
+    }
+}
